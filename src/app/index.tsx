7a71117232e0c8945f61031d6b03eef3419c5d27
Ahmed Nashaat <ahmednashaat.work@gmail.com>
2025-01-19 19:07:21 +0200

    add shadcn

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,33 +1,44 @@
-import { useState } from "react";
 import reactLogo from "../assets/react.svg";
 import viteLogo from "/vite.svg";
 import Provider from "./provider";
+import { Timeline } from "@/components/timeline";
+import { Stepper } from "@/components/stepper";
 
 export function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <Provider>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="flex min-h-[100vh] flex-col items-center justify-center">
+        <div>
+          <a href="https://vite.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <Timeline />
+        <Stepper
+          steps={[
+            {
+              title: "Picked Up",
+              description: "Saturday Nov 10",
+              status: "completed" as const,
+            },
+            {
+              title: "Processing",
+              status: "current" as const,
+            },
+            {
+              title: "Out for Delivery",
+              status: "pending" as const,
+            },
+            {
+              title: "Delivered",
+              status: "pending" as const,
+            },
+          ]}
+        />
       </div>
-      <h1 className="text-red-500">Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </Provider>
   );
 }
